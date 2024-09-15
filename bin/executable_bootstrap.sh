189fb3e964e2b72ce6e6fb1494a12e0f558e7681
Jochen Bauer <devel@jochenbauer.net>
2024-09-15 21:25:01 +0200

    Installing github cli

--- bin/executable_bootstrap.sh
+++ bin/executable_bootstrap.sh
@@ -101,6 +101,15 @@ if command_exists gnome-terminal; then
         rm -rf "$tempdir"
 fi
 
+echo "Installing github cli"
+(type -p wget >/dev/null || (sudo apt update && sudo apt-get install wget -y)) \
+	&& sudo mkdir -p -m 755 /etc/apt/keyrings \
+	&& wget -qO- https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
+	&& sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
+	&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+	&& sudo apt update \
+	&& sudo apt install gh -y
+
 echo "Installing oh-my-zsh. Since we want to do additional install steps afterwards, we won't start a zsh shell automatically."
 RUNZSH=no CHSH=yes sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
