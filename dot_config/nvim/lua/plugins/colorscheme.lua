00368c3402c570d3d758bade245c3a99c79b71b9
Pascal Berndt <drook207@gmail.com>
2026-01-24 23:54:41 +0100

    Rework color scheme

---
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "thesimonho/kanagawa-paper.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+  },
+
+  -- Configure LazyVim to load kanagawa-paper-ink
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "kanagawa-paper-ink",
+    },
+  },
+}
